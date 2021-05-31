b437c5609a31f2e5f7a43956db4786cdc36c70b9
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-05-31 09:57:37 +0100

    app code added to point where aliens should move, but they're not!

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let alienInvadersTakenDown = [];
   let result = 0;
   let direction = 1;
-  let invaderId;
+  let invadersId;
 
   // define the invaders
   const alienInvaders = [
@@ -27,6 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // move the shoooter along a line
   function moveShooter(e) {
+    e.preventDefault();
     squares[currentShooterIndex].classList.remove("shooter");
 
     switch (e.keyCode) {
@@ -43,4 +44,41 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   document.addEventListener("keydown", moveShooter);
+
+  // move alien invaders
+  function moveInvaders() {
+    const leftEdge = alienInvaders[0] % width === 0;
+    const rightEdge =
+      alienInvaders[alienInvaders.length - 1] % width === width - 1;
+
+    if ((leftEdge && direction === -1) || (rightEdge && direction === 1)) {
+      direction = width;
+    } else if (direction === width) {
+      if (leftEdge) direction = 1;
+      else direction = -1;
+    }
+    for (let i = 0; i <= alienInvaders.length - 1; i++) {
+      squares[alienInvaders[i]].classList.remove("invader");
+    }
+    for (let i = 0; i <= alienInvaders.length - 1; i++) {
+      alienInvaders[i] += direction;
+    }
+    for (let i = 0; i <= alienInvaders.length - 1; i++) {
+      squares[alienInvaders[i]].classList.add("invader");
+    }
+
+    // decide if game is over
+    if (squares[currentShooterIndex].classList.contains("invader", "shooter")) {
+      resultDisplay.textContent = "Game Over";
+      squares[currentShooterIndex].classList.add("boom");
+      clearInterval(invaderId);
+    }
+    for (let i = 0; alienInvaders.length - 1; i++) {
+      if (alienInvaders[i] > squares.length - (width - 1)) {
+        resultDisplay.textContent = "Game Over";
+        clearInterval("invaderId");
+      }
+    }
+  }
+  invadersId = setInterval(moveInvaders, 600);
 });
