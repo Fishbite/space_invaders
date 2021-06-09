b2d8197baaf11c6e18b20c23cc6590780f27fa4e
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-06-09 11:47:07 +0100

    Touch screen controls programmed

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,11 @@
       const squares = document.querySelectorAll(".grid div");
       const resultsDisplay = document.querySelector("#result");
 
+      // touch screen controls
+      const lBtn = document.getElementById("leftBtn");
+      const rBtn = document.getElementById("rightBtn");
+      const fBtn = document.getElementById("fireBtn");
+
       let width = 15; // grid width
       let currentShooterIndex = 202; // player start pos
       let currentInvaderIndex = 0; // invader start pos
@@ -36,6 +41,18 @@
         e.preventDefault();
         squares[currentShooterIndex].classList.remove("shooter");
 
+        console.log(e);
+
+        switch (e.target) {
+          case lBtn:
+            if (currentShooterIndex % width !== 0) currentShooterIndex -= 1;
+            break;
+          case rBtn:
+            if (currentShooterIndex % width < width - 1)
+              currentShooterIndex += 1;
+            break;
+        }
+
         switch (e.keyCode) {
           case 37:
             if (currentShooterIndex % width !== 0) currentShooterIndex -= 1;
@@ -45,10 +62,18 @@
               currentShooterIndex += 1;
             break;
         }
+
+        if (lBtn) {
+          if (currentShooterIndex % width < -1) width -= 1;
+        }
+
         squares[currentShooterIndex].classList.add("shooter");
       }
 
       document.addEventListener("keydown", moveShooter);
+
+      lBtn.addEventListener("touchstart", moveShooter);
+      rBtn.addEventListener("touchstart", moveShooter);
 
       // move alien invaders
       function moveInvaders() {
@@ -132,11 +157,17 @@
             }
           }
         }
-        switch (e.key) {
+        switch (e.key || e.target) {
           case "ArrowUp":
+            laserId = setInterval(moveLaser, 100);
+            break;
+        }
+        switch (e.target) {
+          case fBtn:
             laserId = setInterval(moveLaser, 100);
         }
       }
+      fBtn.addEventListener("touchstart", shoot);
 
       document.addEventListener("keydown", shoot);
     }
